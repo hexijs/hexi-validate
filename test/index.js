8164c8d6c993f5554f1a2ed58bf8c6e209064c52
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-01-24 20:27:00 +0200

    unit test added

--- test/index.js
+++ test/index.js
@@ -4,12 +4,19 @@ const hexi = require('hexi')
 const hexiValidate = require('..')
 const request = require('supertest')
 const joi = require('joi')
+const bodyParser = require('body-parser')
 
 describe('auth', function() {
   let server
 
   beforeEach(function(next) {
     server = new hexi.Server()
+
+    server.express.use(bodyParser.json())
+    server.express.use(bodyParser.urlencoded({
+      extended: true,
+    }))
+
     server.register([hexiValidate], next)
   })
 
@@ -35,13 +42,36 @@ describe('auth', function() {
       .expect(200, done)
   })
 
-  it('should return error if validation fails', function(done) {
+  it('should return error if headers validation fails', function(done) {
     server.route({
       path: '/',
       method: 'GET',
       config: {
         validate: {
-          headers: joi.any().valid('bar'),
+          headers: {
+            foo: joi.string().required(),
+          },
+        },
+      },
+      handler(req, res) {
+        res.status(200).end()
+      },
+    })
+
+    request(server.express)
+      .get('/')
+      .expect(400, done)
+  })
+
+  it('should return error if body validation fails', function(done) {
+    server.route({
+      path: '/',
+      method: 'GET',
+      config: {
+        validate: {
+          body: {
+            foo: joi.string().required(),
+          },
         },
       },
       handler(req, res) {
